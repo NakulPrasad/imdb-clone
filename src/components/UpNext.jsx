034a43a275ce09796244a545f790e9828b757af6
Nakul Prasad <nakulprasad10@outlook.com>
2023-07-11 00:35:04 +0530

    Up Next finished styling

--- src/components/UpNext.jsx
+++ src/components/UpNext.jsx
@@ -1,27 +1,48 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Typography, styled } from "@mui/material"
 
+
+const StyledBox = styled(Box)`
+width: 35%; 
+padding-left: 18px;
+display: flex;
+flex-direction: column;
+align-items: baseline;
+& > p {
+    font-weight : 600;
+    font-size : 20px;
+    margin-bottom :10px;
+    color: #F5C518;
+}
+`
+const StyledBox2 = styled(Box)`
+display: flex;
+& > p {
+    margin-left: 20px;
+
+}
+`
 
 const UpNext = ({ movies }) => {
     console.log(movies);
     return (
-        <Box style={{ width: "35%", padding: '0 12px' }}>
-            <Typography>UP Next</Typography>
+        <StyledBox>
+            <Typography>Up Next</Typography>
             {
 
                 movies.splice(0, 3).map(function (movie) {
                     return (
-                        <Box style={{ display: 'flex' }}>
+                        <StyledBox2>
                             <img src={`https://image.tmdb.org/t/p/w92/${movie.poster_path}`}
                                 alt="poster"
 
                             />
                             <Typography>{movie.original_title}</Typography>
-                        </Box>
+                        </StyledBox2>
                     );
                 })
 
             }
-        </Box>
+        </StyledBox>
     )
 }
 
