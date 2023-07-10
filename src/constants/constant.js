b0aa6fed88f32eca2d7369639fd2f9543a754ea0
Nakul Prasad <nakulprasad10@outlook.com>
2023-07-10 14:30:55 +0530

    APIs Added

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,1 +1,6 @@
 export const logoURL = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png'
+
+export const POPULAR_API_URL = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=a919d421096e061f264058805fcfbac3'
+export const UPCOMING_API_URL = 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1&api_key=a919d421096e061f264058805fcfbac3'
+export const TOPRATED_API_URL = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=a919d421096e061f264058805fcfbac3'
+export const NOWPLAYING_API_URL = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=a919d421096e061f264058805fcfbac3'
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import Header from '../components/common/Header'
+import { categoryMovies } from '../services/api'
+import { NOWPLAYING_API_URL } from '../constants/constant'
+import { Box } from '@mui/icons-material';
 
 const Home = () => {
+    //store response from calling api  in state;
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        const getData = async () => {
+            let response = await categoryMovies(NOWPLAYING_API_URL);//calling api at load
+            setMovies(response.results);//insert response
+        }
+        getData();
+    }, [])
+
+
     return (
-        <div>
+        <>
             <Header />
-        </div>
+            <Box>
+                <Banner />
+            </Box>
+        </>
     )
 }
 
