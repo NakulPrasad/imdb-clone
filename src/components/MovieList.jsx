d6b0f8934a61d1d0e8b6630940c4757be6adadaf
Nakul Prasad <nakulprasad10@outlook.com>
2023-07-25 00:22:52 +0530

    update

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,47 +2,37 @@ import { Star } from "@mui/icons-material";
 import { styled, List, ListItem, Divider } from "@mui/material";
 
 const Container = styled(List)`
-display : flex;
-
-`
-
+  display: flex;
+`;
 
 const MovieList = ({ movies }) => {
+  return (
+    <>
+      {movies.map(function (movie) {
+        return (
+          <Container>
+            <ListItem>
+              <img
+                src={`https://image.tmdb.org/t/p/w92/${movie.poster_path}`}
+                alt="poster"
+              />
+            </ListItem>
+            <ListItem>
+              <sTypography>{movie.title}</sTypography>
+            </ListItem>
+            <ListItem>
+              <Star color={"warning"} />
+              <sTypography>{movie.vote_average} </sTypography>
+            </ListItem>
+            <ListItem>
+              <sTypography>{movie.release_date}</sTypography>
+            </ListItem>
+            <Divider />
+          </Container>
+        );
+      })}
+    </>
+  );
+};
 
-
-    return (
-        <>
-            {
-                movies.map(function (movie) {
-                    return (
-                        <Container>
-                            <ListItem >
-
-                                <img src={`https://image.tmdb.org/t/p/w92/${movie.poster_path}`}
-                                    alt="poster"
-
-                                />
-                            </ListItem>
-                            <ListItem>
-
-                                <sTypography>{movie.title}</sTypography>
-
-                            </ListItem>
-                            <ListItem>
-                                <Star color={'warning'} />
-                                <sTypography >{movie.vote_average} </sTypography>
-                            </ListItem>
-                            <ListItem>
-                                <sTypography >{movie.release_date}</sTypography>
-                            </ListItem>
-                            <Divider />
-                        </Container>
-                    );
-                })
-
-            }
-        </>
-    )
-}
-
-export default MovieList
+export default MovieList;
